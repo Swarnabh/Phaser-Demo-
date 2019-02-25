942415d8bacac23ec1cb18f39ae7da9a8940fa75
Agrawal <swarnabh.agrawal@igt.com>
2019-02-25 17:40:54 +0530

    text added

--- state6.js
+++ state6.js
@@ -1,10 +1,22 @@
+//Using text and firebase
+
+var hsText = [],
+  hs = [16, 32, 73, 43, 15, 86, 72, 58, 89, 69];
 Game.state6 = function() {};
 Game.state6.prototype = {
   preload: function() {},
   create: function() {
-    game.stage.backgroundColor = "#0280fd";
+    game.stage.backgroundColor = "#eeeeee";
 
     addChangeStateEventListeners();
+    for (var i = 1; i < 11; i++) {
+      game.add
+        .text(500, 30 + i * 90, i + ". ", { fontSize: "40px" })
+        .anchor.setTo(1, 0);
+    }
+    for (var i = 0; i < 10; i++) {
+      game.add.text(500, 30 + (i + 1) * 90, hs[i], { fontSize: "40px" });
+    }
   },
   update: function() {}
 };
