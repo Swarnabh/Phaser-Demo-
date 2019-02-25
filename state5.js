3db0b542f0e2a9e383124b15398ab4e84676f1a2
Agrawal <swarnabh.agrawal@igt.com>
2019-02-25 16:03:55 +0530

    Touch screen swipe added to state5

--- state5.js
+++ state5.js
@@ -1,10 +1,46 @@
+//Touch screen swipes
+var arrow;
+var startX, startY, endX, endY, swipe;
 Game.state5 = function() {};
 Game.state5.prototype = {
-  preload: function() {},
+  preload: function() {
+    game.load.image("arrow", "assets/images/arrow.png");
+  },
   create: function() {
     game.stage.backgroundColor = "#42490b";
-
+    arrow = game.add.sprite(centerX, centerY, "arrow");
+    arrow.anchor.setTo(0.5);
     addChangeStateEventListeners();
+    game.input.onDown.add(this.startswipe);
+    game.input.onUp.add(this.endSwipe);
   },
-  update: function() {}
+  update: function() {},
+  startswipe: function() {
+    startX = game.input.x;
+    startY = game.input.y;
+  },
+  endSwipe: function() {
+    endX = game.input.x;
+    endY = game.input.y;
+    if (Math.abs(endX - startX) < 10 && Math.abs(endY - startY) < 10) {
+      return false;
+    }
+    if (Math.abs(endX - startX) > Math.abs(endY - startY)) {
+      console.log("Horizontal");
+      if (endX > startX) {
+        swipe = 90;
+      } else if (endX < startX) {
+        swipe = 270;
+      }
+    }
+    if (Math.abs(endX - startX) < Math.abs(endY - startY)) {
+      console.log("Vertical");
+      if (endY > startY) {
+        swipe = 180;
+      } else if (endY < startY) {
+        swipe = 0;
+      }
+    }
+    arrow.angle = swipe;
+  }
 };
