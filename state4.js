b539ecf6cf37cf2c5a1bb35b84b2a6e09fc3da42
Agrawal <swarnabh.agrawal@igt.com>
2019-02-25 13:22:13 +0530

    Gravity and Acceleration implemented in State 4

--- state4.js
+++ state4.js
@@ -1,10 +1,28 @@
+//Gravity and Acceleration
+accel = 400;
 Game.state4 = function() {};
 Game.state4.prototype = {
   preload: function() {},
   create: function() {
     game.stage.backgroundColor = "#835212";
-
+    adam = game.add.sprite(300, 300, "adam");
+    game.physics.enable(adam);
+    adam.body.gravity.y = 500;
+    adam.body.bounce.y = 0.3;
+    adam.body.drag.x = 400;
+    adam.body.collideWorldBounds = true;
     addChangeStateEventListeners();
   },
-  update: function() {}
+  update: function() {
+    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+      adam.body.acceleration.x = -accel;
+    } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+      adam.body.acceleration.x = accel;
+    } else {
+      adam.body.acceleration.x = 0;
+    }
+    if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+      adam.body.velocity.y = -400;
+    }
+  }
 };
